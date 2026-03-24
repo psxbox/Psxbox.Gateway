6e6325cf0eb222844384bd5e960e5cdf11781109
Adham Yusupov <adhamyu83@gmail.com>
2026-03-24 15:03:38 +0500

    Добавить свойство MaxConcurrentPublish в класс GatewayInfo

--- Psxbox.TBGateway/GatewayInfo.cs
+++ Psxbox.TBGateway/GatewayInfo.cs
@@ -5,5 +5,6 @@
         public string? Token { get; set; }
         public string? Host { get; set; }
         public int Port { get; set; } = 1883;
+        public int MaxConcurrentPublish { get; set; } = 100;
     }
 }
